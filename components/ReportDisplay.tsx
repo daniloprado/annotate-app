5797b3f64f5d2bbf30d131b584e117392e8321b7
daniloprado <daniloprado@users.noreply.github.com>
2025-12-11 17:39:03 +0000

    Add files via upload

---
+++ components/ReportDisplay.tsx
@@ -0,0 +1,6 @@
+
+import React, { useState, useRef, useEffect } from 'react';
+import { AnalysisResult, Issue, Annotation } from '../types';
+import { EditIcon } from './icons/EditIcon';
+import { PlusIcon } from './icons/PlusIcon';
+import { TrashIcon } from './icons/TrashIcon';
